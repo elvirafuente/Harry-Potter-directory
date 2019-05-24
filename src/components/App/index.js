5b4555a751d29d3b45a9b39e75ff33c5be0bec48
Elvira Fuente <hebebicho@gmail.com>
2019-05-24 16:19:15 +0200

    add fetch service

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,31 @@
-import React, {Fragment, Component} from 'react';
+import React, { Fragment, Component } from 'react';
 import './styles.css';
 // import { Route, Switch } from 'react-router-dom';
 import MainPage from '../MainPage'
 
 class App extends Component {
-  render(){
+  constructor(props) {
+    super(props)
+
+  }
+
+  componentDidMount() {
+    this.fetchService();
+
+  }
+
+  fetchService() {
+    const url = "http://hp-api.herokuapp.com/api/characters";
+    return (
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+        })
+    )
+  }
+
+  render() {
     return (
       <Fragment >
         <header className="page__header">

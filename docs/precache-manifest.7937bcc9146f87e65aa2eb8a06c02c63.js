9d7397e348e70ca9c3257865698062e441d8983a
Elvira Fuente <hebebicho@gmail.com>
2019-05-27 20:00:30 +0200

    add docs folder

---
+++ docs/precache-manifest.7937bcc9146f87e65aa2eb8a06c02c63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e26e9c7a72d0c85bc2908f09eabfc9b2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6e42564a8dfe89dbf4d1",
+    "url": "./static/css/main.56efbd3a.chunk.css"
+  },
+  {
+    "revision": "f36ffa42bea37505f3a0",
+    "url": "./static/js/2.cf653cb2.chunk.js"
+  },
+  {
+    "revision": "6e42564a8dfe89dbf4d1",
+    "url": "./static/js/main.097d7a25.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "f69c832b6ca2fc7737812f16872ee86c",
+    "url": "./static/media/background-castle.f69c832b.jpg"
+  }
+]);

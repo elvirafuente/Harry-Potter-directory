6480cce85002a1ebba1e1c64b1c520087c36a90b
Elvira Fuente <hebebicho@gmail.com>
2019-05-27 20:07:52 +0200

    add docs folder

---
+++ docs/build/precache-manifest.73430c7356861c5b4eebf13d9c906a5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5c76b5c5b2ebe37c07643d1f5e37ab3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c1172c2ff7658c53c53d",
+    "url": "./static/css/main.aa08f2d6.chunk.css"
+  },
+  {
+    "revision": "f36ffa42bea37505f3a0",
+    "url": "./static/js/2.cf653cb2.chunk.js"
+  },
+  {
+    "revision": "c1172c2ff7658c53c53d",
+    "url": "./static/js/main.060da72c.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "f69c832b6ca2fc7737812f16872ee86c",
+    "url": "./static/media/background-castle.f69c832b.jpg"
+  }
+]);
--- src/components/MainPage/index.js
+++ src/components/MainPage/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './styles.css';
+import './styles.scss';
 import Filters from '../Filters'
 import CharacterList from '../CharacterList';
 
 class MainPage extends React.Component {
   render(){
     return (
-      <main>
+      <main className="page__main">
         <nav>
           <Filters 
             handleInputName={this.props.handleInputName}
